4ed29b1d0957174636613a3275692feb9e402e4d
Bastion Fennell <bastion.g.fennell@gmail.com>
2016-12-22 19:31:41 -0600

    Make the selected styling look nicer

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -12,14 +12,32 @@ export default React.createClass({
   hasVotedFor(entry) {
     return this.props.hasVoted === entry;
   },
+  hasVotedAgainst(entry) {
+    return this.props.hasVoted && !this.hasVotedFor(entry);
+  },
+  getClassname(entry) {
+    const entryClass = entry.toLowerCase().replace(/\s/g, '-', 'g');
+    let votedClass = '';
+
+    if(this.hasVotedFor(entry)) {
+      votedClass = 'voted-for';
+    }
+    else if(this.hasVotedAgainst(entry)) {
+      votedClass = 'voted-against';
+    }
+
+    return `${entryClass} ${votedClass}`;
+  },
   render() {
     return <div className="voting mdl-grid">
       {this.getPair().map(entry =>
-        <button   className="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--6-col"
+        <button   className={`mdl-card mdl-shadow--2dp mdl-cell mdl-cell--6-col background-image ${this.getClassname(entry)}`}
                   key={entry}
                   disabled={this.isDisabled()}
                   onClick={() => this.props.vote(entry)}>
-            <h1>{entry}</h1>
+            <div className="background-image__text">
+              <h2>{entry}</h2>
+            </div>
             {this.hasVotedFor(entry) ?
               <div className="label">Voted</div> :
               null}
--- src/index.js
+++ src/index.js
@@ -10,9 +10,12 @@ import io from 'socket.io-client';
 import {Router, Route, hashHistory} from 'react-router';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
+
 import './styles/index.css';
 import './styles/voting.css';
+import './styles/background-images.css';
 import './styles/material.min.css';
+
 import './material.min.js';
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
